e9f7218ceb0bc7d99ab1f1f0d744b6bb7463a107
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-07-22 14:37:38 +0300

    Update splash_screen.dart

--- Flutter/lib/screens/splash_screen.dart
+++ Flutter/lib/screens/splash_screen.dart
@@ -1,1 +1,96 @@
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const DashboardScreen(),
+          ),
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      backgroundColor: Colors.blue,
+
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+
+            const Icon(
+              Icons.favorite,
+              color: Colors.white,
+              size: 90,
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+
+              "ESP Health Monitoring",
+
+              style: TextStyle(
+
+                color: Colors.white,
+
+                fontSize: 26,
+
+                fontWeight: FontWeight.bold,
+
+              ),
+
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text(
+
+              "AI Medical System",
+
+              style: TextStyle(
+
+                color: Colors.white70,
+
+                fontSize: 18,
+
+              ),
+
+            ),
+
+          ],
+
+        ),
+
+      ),
+
+    );
+
+  }
+}
