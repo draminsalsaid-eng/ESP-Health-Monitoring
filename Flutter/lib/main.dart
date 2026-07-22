b4031132bd41a74b8588761ea33d8075e9d94e5b
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-07-22 14:50:19 +0300

    Update main.dart

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,65 +1,45 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+
 
 void main() {
-  runApp(const ESPHealthApp());
+
+  runApp(
+    const ESPHealthApp(),
+  );
+
 }
 
+
+
 class ESPHealthApp extends StatelessWidget {
+
   const ESPHealthApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       title: 'ESP Health Monitoring',
 
       debugShowCheckedModeBanner: false,
 
+
       theme: ThemeData(
+
         primarySwatch: Colors.blue,
-      ),
 
-      home: const HomePage(),
-    );
-  }
-}
-
-
-class HomePage extends StatelessWidget {
-
-  const HomePage({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      appBar: AppBar(
-        title: const Text(
-          "ESP Health Monitoring",
-        ),
-      ),
-
-
-      body: const Center(
-
-        child: Text(
-
-          "System Ready",
-
-          style: TextStyle(
-
-            fontSize: 24,
-
-            fontWeight: FontWeight.bold,
-
-          ),
-
-        ),
+        useMaterial3: true,
 
       ),
+
+
+      home: const SplashScreen(),
 
     );
 
   }
+
 }
