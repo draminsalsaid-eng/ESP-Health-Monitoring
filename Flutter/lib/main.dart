15e23a9b299decce1ea1fca6c6ed4d3383611044
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-07-22 13:57:35 +0300

    Update main.dart

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,1 +1,65 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const ESPHealthApp());
+}
+
+class ESPHealthApp extends StatelessWidget {
+  const ESPHealthApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ESP Health Monitoring',
+
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      home: const HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+
+  const HomePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text(
+          "ESP Health Monitoring",
+        ),
+      ),
+
+
+      body: const Center(
+
+        child: Text(
+
+          "System Ready",
+
+          style: TextStyle(
+
+            fontSize: 24,
+
+            fontWeight: FontWeight.bold,
+
+          ),
+
+        ),
+
+      ),
+
+    );
+
+  }
+}
