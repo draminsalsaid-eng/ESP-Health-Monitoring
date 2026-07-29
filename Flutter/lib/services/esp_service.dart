8b06544df25516e9279f8b78bcc5697c37faf11f
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-07-29 17:50:23 +0300

    Update esp_service.dart

--- Flutter/lib/services/esp_service.dart
+++ Flutter/lib/services/esp_service.dart
@@ -8,5 +8,52 @@ import 'api_constants.dart';
 import 'network_exception.dart';
 
 class ESPService {
+  // Check Connection  //
+Future<bool> checkConnection() async {
+  try {
+    final response = await http
+        .get(
+          Uri.parse(
+            ApiConstants.baseUrl + ApiConstants.health,
+          ),
+        )
+        .timeout(ApiConstants.connectionTimeout);
 
+    return response.statusCode == 200;
+  } catch (_) {
+    return false;
+  }
+}
+  // Get health Data  //
+  Future<HealthResponse> getHealthData() async {
+  try {
+    final response = await http
+        .get(
+          Uri.parse(
+            ApiConstants.baseUrl + ApiConstants.health,
+          ),
+        )
+        .timeout(ApiConstants.receiveTimeout);
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+
+      return HealthResponse.fromJson(jsonData);
+    } else {
+      throw NetworkException(
+        'ESP32 returned error: ${response.statusCode}',
+      );
+    }
+  } catch (e) {
+    if (e is NetworkException) {
+      rethrow;
+    }
+
+    throw NetworkException(
+      'Failed to get health data from ESP32',
+    );
+  }
+}
+// Check Connection  //
+  
 }
