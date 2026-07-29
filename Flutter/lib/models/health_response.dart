cfb2c51d84952800cedbbffb34901cb23a07bcf9
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-07-29 15:02:02 +0300

    Create health_response.dart

---
+++ Flutter/lib/models/health_response.dart
@@ -0,0 +1,23 @@
+import 'user_input.dart';
+import 'sensor_data.dart';
+import 'ai_result.dart';
+
+class HealthResponse {
+  final UserInput userInput;
+  final SensorData sensorData;
+  final AIResult aiResult;
+
+  const HealthResponse({
+    required this.userInput,
+    required this.sensorData,
+    required this.aiResult,
+  });
+
+  factory HealthResponse.fromJson(Map<String, dynamic> json) {
+    return HealthResponse(
+      userInput: UserInput.fromJson(json),
+      sensorData: SensorData.fromJson(json),
+      aiResult: AIResult.fromJson(json),
+    );
+  }
+}
