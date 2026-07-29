ad36891e2e02572b27c3d5b06251f124d79475cd
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-07-29 14:58:52 +0300

    Create user_input.dart

---
+++ Flutter/lib/models/user_input.dart
@@ -0,0 +1,27 @@
+class UserInput {
+  final String workerType;
+  final String activity;
+  final String environment;
+
+  const UserInput({
+    required this.workerType,
+    required this.activity,
+    required this.environment,
+  });
+
+  factory UserInput.fromJson(Map<String, dynamic> json) {
+    return UserInput(
+      workerType: json['worker_type'] ?? '',
+      activity: json['activity'] ?? '',
+      environment: json['environment'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'worker_type': workerType,
+      'activity': activity,
+      'environment': environment,
+    };
+  }
+}
