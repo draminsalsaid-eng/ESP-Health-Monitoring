d11be8c42c62e48149b62d3a489193cf46029cc1
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-08-05 20:34:16 +0300

    Create worker_constants.dart

---
+++ Flutter/lib/constants/worker_constants.dart
@@ -0,0 +1,39 @@
+const List<String> workerTypes = [
+
+  "young",
+  "senior",
+  "smoker",
+  "experienced",
+  "athlete",
+  "heavy_worker",
+
+];
+
+const List<String> activities = [
+
+  "walking",
+  "grinding",
+  "hammer_drilling",
+  "heavy_lifting",
+  "carrying_tools",
+  "confined_space",
+  "gas_inspection",
+  "idle_monitoring",
+  "rest_break",
+  "emergency_escape",
+  "climbing",
+
+];
+
+const List<String> environments = [
+
+  "steel_factory",
+  "chemical_plant",
+  "warehouse",
+  "construction",
+  "mining",
+  "oil_gas",
+  "outdoor",
+  "tunnel",
+
+];
