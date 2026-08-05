0ebda6e573f4aab00593816f17aa1bb4c7fba749
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-08-05 19:45:02 +0300

    Update user_input.dart

--- Flutter/lib/models/user_input.dart
+++ Flutter/lib/models/user_input.dart
@@ -2,73 +2,21 @@ class UserInput {
   final String userId;
   final String workerType;
   final String activity;
-  final String environment;
+  final String workplace;
 
-  const UserInput({
-
+  UserInput({
     required this.userId,
     required this.workerType,
     required this.activity,
-    required this.environment,
-
+    required this.workplace,
   });
 
-  factory UserInput.fromJson(
-      Map<String, dynamic> json) {
-
-    return UserInput(
-
-      userId:
-          json['user_id'] ?? '',
-
-      workerType:
-          json['worker_type'] ?? '',
-
-
-      activity:
-          json['activity'] ?? '',
-
-
-      environment:
-          json['environment'] ?? '',
-
-
-    );
-
-
-  }
-
-
-
-
-
   Map<String, dynamic> toJson() {
-
-
     return {
-
-
-      'user_id':
-          userId,
-
-
-      'worker_type':
-          workerType,
-
-
-      'activity':
-          activity,
-
-
-      'environment':
-          environment,
-
-
+      "user_id": userId,
+      "worker_type": workerType,
+      "activity": activity,
+      "workplace": workplace,
     };
-
-
   }
-
-
 }
-
