7cab40ee39ccf18fe13e116c8939d1195527c25c
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-08-05 22:04:47 +0300

    Update user_input.dart

--- Flutter/lib/models/user_input.dart
+++ Flutter/lib/models/user_input.dart
@@ -1,20 +1,35 @@
  class UserInput {
-  final String userId;
+
   final String workerType;
   final String activity;
   final String environment;
+
+
   UserInput({
-    required this.userId,
+
     required this.workerType,
+
     required this.activity,
+
     required this.environment,
+
   });
+
+
+
   Map<String,dynamic> toJson(){
+
     return {
-      "user_id": userId,
+
       "worker_type": workerType,
+
       "activity": activity,
+
       "environment": environment,
+
     };
+
   }
+
+
 }
