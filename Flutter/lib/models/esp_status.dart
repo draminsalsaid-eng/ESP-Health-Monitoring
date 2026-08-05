b848304d20f33c91d339a5e6c2e7f67ea1ddf968
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-08-05 16:47:17 +0300

    Update esp_status.dart

--- Flutter/lib/models/esp_status.dart
+++ Flutter/lib/models/esp_status.dart
@@ -16,31 +16,129 @@ enum ESPStatus {
 
 
 
-ESPStatus parseESPStatus(String value) {
+class ESPState {
 
 
-  switch(value) {
+  final ESPStatus status;
+
+  final String message;
 
 
-    case 'waiting_finger':
-      return ESPStatus.waitingFinger;
+
+  const ESPState({
+
+    required this.status,
+
+    required this.message,
+
+  });
 
 
-    case 'measuring':
-      return ESPStatus.measuring;
 
 
-    case 'processing_ai':
-      return ESPStatus.processingAI;
+
+  factory ESPState.fromJson(
+      Map<String,dynamic> json,
+  ){
+
+    final String value =
+        json['status'] ?? 'idle';
 
 
-    case 'completed':
-      return ESPStatus.completed;
 
 
-    default:
-      return ESPStatus.idle;
+    switch(value){
+
+
+      case 'waiting_finger':
+
+        return const ESPState(
+
+          status: ESPStatus.waitingFinger,
+
+          message:
+          'Place finger on MAX30105',
+
+        );
+
+
+
+
+      case 'measuring':
+
+        return const ESPState(
+
+          status: ESPStatus.measuring,
+
+          message:
+          'Reading sensors',
+
+        );
+
+
+
+
+
+      case 'processing_ai':
+
+        return const ESPState(
+
+          status: ESPStatus.processingAI,
+
+          message:
+          'Analyzing health data',
+
+        );
+
+
+
+
+
+      case 'completed':
+
+        return const ESPState(
+
+          status: ESPStatus.completed,
+
+          message:
+          'Health analysis completed',
+
+        );
+
+
+
+
+
+      case 'error':
+
+        return const ESPState(
+
+          status: ESPStatus.error,
+
+          message:
+          'ESP32 error',
+
+        );
+
+
+
+
+
+      default:
+
+        return const ESPState(
+
+          status: ESPStatus.idle,
+
+          message:
+          'Ready',
+
+        );
+
+    }
 
   }
+
+
 
 }
