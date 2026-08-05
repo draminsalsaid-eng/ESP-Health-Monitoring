3479eb5dd7878068942552fcf04cfd5364743b65
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-08-05 15:55:33 +0300

    Update login_screen.dart

--- Flutter/lib/screens/login_screen.dart
+++ Flutter/lib/screens/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth_provider.dart';
-import 'dashboard_screen.dart';
+import 'home_navigation.dart';
 
 
 
