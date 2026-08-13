3bd754ea4685678f6efef4e7be7ca15ca9c781bf
draminsalsaid-eng <draminsalsaid@gmail.com>
2026-08-13 20:13:42 +0300

    Update user_input.dart

--- Flutter/lib/models/user_input.dart
+++ Flutter/lib/models/user_input.dart
@@ -1,67 +1,35 @@
 class UserInput {
-
   final String userId;
 
   final String workerType;
-
   final String activity;
-
   final String environment;
 
-
-
   UserInput({
-
     required this.userId,
-
     required this.workerType,
-
     required this.activity,
-
     required this.environment,
-
   });
 
-
-
+  // البيانات التي يمكن إرسالها إلى ESP32 / API
+  // User ID لا يتم إرسالها.
   Map<String, dynamic> toJson() {
-
     return {
-
-      "user_id": userId,
-
       "worker_type": workerType,
-
       "activity": activity,
-
       "environment": environment,
-
     };
-
   }
-
-
 
   factory UserInput.fromJson(
-      Map<String, dynamic> json,
+    Map<String, dynamic> json,
   ) {
-
     return UserInput(
-
-      userId:
-          json["user_id"] ?? "",
-
-      workerType:
-          json["worker_type"] ?? "",
-
-      activity:
-          json["activity"] ?? "",
-
-      environment:
-          json["environment"] ?? "",
-
+      userId: json["user_id"] ?? "",
+      workerType: json["worker_type"] ?? "",
+      activity: json["activity"] ?? "",
+      environment: json["environment"] ?? "",
     );
-
   }
-
 }
